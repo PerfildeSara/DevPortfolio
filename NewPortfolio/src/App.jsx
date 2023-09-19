16f219ef4fea0941962cd66f4dc84dc5edf6bacd
PerfildeSara <saradesarrolladora@gmail.com>
2023-09-18 20:23:41 -0500

    Work in progress, missing responsive design  and a 'two lies and one lie' pag.

--- NewPortfolio/src/App.jsx
+++ NewPortfolio/src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header' 
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Riddle from './pages/Riddle'
+import AboutMe from './pages/AboutMe'
+import Experience from './pages/Experience'
+import ContactMe from './pages/ContactMe'
+import Projects from './pages/Projects'
+import Home from './pages/Home'
+import Footer from './components/Footer'
+import { routes } from './Routes'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header></Header>
+      <Routes>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.aboutMe} element={<AboutMe />} />
+        
+        <Route path={routes.experience} element={<Experience />} />
+        <Route path={routes.projects} element={<Projects />} />
+        <Route path={routes.twoThruthsAndOneLie} element={<Riddle/>} />
+        <Route path={routes.contact} element={<ContactMe />} />
+      </Routes>
+      <Footer></Footer>
     </>
   )
 }
